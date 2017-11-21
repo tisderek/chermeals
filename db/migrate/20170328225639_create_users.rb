c0eeb32f1c7e383742848063814db7dc5b2fcaba
Derek Gerson <hi@derekgerson.com>
2017-11-21 09:04:50 -0800

    make group optional

--- db/migrate/20170328225639_create_users.rb
+++ db/migrate/20170328225639_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
       t.string            :name, null: false, default: ""
-      t.belongs_to        :group
+      t.belongs_to        :group, optional: true
 
       t.timestamps
     end
