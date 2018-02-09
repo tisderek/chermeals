44c3284d34142c8f4e9e1638fa2df6e009d3a1d7
Derek Gerson <hi@derekgerson.com>
2018-02-08 21:39:33 -0800

    implement Gift model from/to aliases for provider/receiver rels

--- app/models/gift.rb
+++ app/models/gift.rb
@@ -2,4 +2,7 @@ class Gift < ApplicationRecord
   belongs_to :provider, class_name: 'Member'
   belongs_to :receiver, class_name: 'Member'
   belongs_to :meal
+
+  alias_attribute :from, :provider
+  alias_attribute :to, :receiver
 end
