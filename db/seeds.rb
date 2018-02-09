597b783d5571da7f5d51fdf7dd86e435f2214ebb
Derek Gerson <hi@derekgerson.com>
2018-02-08 21:39:58 -0800

    rework seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,36 +1,38 @@
-derek = Member.create(name: 'Derek', email:'test@test.com', password: 'password')
-jack = Member.create(name: 'Jack', email:'test2@test.com', password: 'password')
-nly = Group.create(name: 'Neighbors', slug:'neighbors')
+derek = Member.create(name: 'Derek', email:'derek@google.com', password: 'password')
+jack = Member.create(name: 'Jack', email:'jack@google.com', password: 'password')
+faye = Member.create(name: 'Faye', email:'faye@google.com', password: 'password')
+
+nly = Group.create(name: 'Neighbors', slug:'google.com')
+
 nly.members << derek
 nly.members << jack
-sandy = Member.create(name: 'Faye', email:'sandy@neighborly.com', password: 'password')
+nly.members << faye
 
-stew = Meal.create(
+stew = Meal.create \
   main_title: 'Spanish-Style Potato & Chickpea Stew',
   expected_date: Date.new(2018, 06, 10),
   vendor: "Blue Apron"
-)
 
-derek.meals << stew
-
-cod = jack.provided_meals.create(
+cod = Meal.create \
   main_title: 'Seared Cod & Crème Fraîche Sauce',
   expected_date: Date.new(2018, 05, 20),
   vendor: "Blue Apron"
-)
 
-sandwich = jack.provided_meals.create(
+
+sandwich = Meal.create \
   main_title: 'Spiced Beef Sandwiches & Tzatziki Sauce',
   expected_date: Date.new(2018, 05, 10),
   vendor: "Blue Apron"
-)
 
-catfish = derek.provided_meals.create(
+catfish = Meal.create \
   main_title: 'Seared Catfish',
   expected_date: Date.new(2018, 06, 01),
   vendor: "Blue Apron"
-)
 
-derek.provided_gifts.create(receiver_id: sandy.id, meal_id: stew.id),
-derek.received_gifts.create(vendor_id: jack.id, meal_id: sandwich.id)
+derek.meals << stew
+jack.meals << cod
+jack.meals << sandwich
+jack.meals << catfish
 
+Gift.create(from: derek, to: faye, meal: stew)
+Gift.create(from: jack, to: derek, meal: cod)
